8d939df76708b4981cc6e5ddf327ff0d96e80b71
Dickson Lee <dicksonT_lee@hotmail.com>
2025-06-23 00:58:43 -0400

    fixed query

--- sql/table-setup.sql
+++ sql/table-setup.sql
@@ -38,5 +38,4 @@ CREATE TABLE menu_img_descriptions (
 INSERT INTO menu_categories (category_name) VALUES 
 ('Combo'), ('Main Dishes'), ('Soup & Noodles'), ('Specials & Extras'), ('Desserts & Drinks');
 
-ALTER TABLE menu_descriptions
-ADD CONSTRAINT unique_item_lang UNIQUE (item_id, lang_code);
+ALTER TABLE menu_descriptions ADD CONSTRAINT unique_item_lang UNIQUE (item_id, lang_code);
